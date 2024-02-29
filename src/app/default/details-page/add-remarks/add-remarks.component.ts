a6a43ab4a8f0d431fb52b70a6306fdbcd7b232f9
Sirajudeen <ksiraj2002@gmail.com>
2024-02-29 22:20:41 +0530

    comments page

---
+++ src/app/default/details-page/add-remarks/add-remarks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-remarks',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-remarks.component.html',
+  styleUrl: './add-remarks.component.scss'
+})
+export class AddRemarksComponent {
+
+}
--- src/app/default/details-page/details-page.component.ts
+++ src/app/default/details-page/details-page.component.ts
@@ -3,11 +3,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {  TabAlignment, TabComponent } from "@progress/kendo-angular-layout";
 import { LayoutModule } from "@progress/kendo-angular-layout";
 import { SalesInfoTabComponent } from './sales-info-tab/sales-info-tab.component';
+import { AddRemarksComponent } from './add-remarks/add-remarks.component';
 
 @Component({
   selector: 'app-details-page',
   standalone: true,
-  imports: [LayoutModule, ReactiveFormsModule, SalesInfoTabComponent, FormsModule],
+  imports: [LayoutModule, ReactiveFormsModule, SalesInfoTabComponent, AddRemarksComponent,  FormsModule],
   templateUrl: './details-page.component.html',
   styleUrl: './details-page.component.scss'
 })
