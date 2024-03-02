8cb11a443f52b482e9c2972f23456f61308d1a3d
Yekula <ramadevi.yekula@acgov.org>
2024-03-02 08:57:40 -0800

    puhsed new changes

--- src/app/default/details-page/add-remarks/add-remarks.component.ts
+++ src/app/default/details-page/add-remarks/add-remarks.component.ts
@@ -8,27 +8,27 @@ import {
   DialogRef
 } from '@progress/kendo-angular-dialog';
 import { InputsModule } from '@progress/kendo-angular-inputs';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+import { DialogThemeColor } from "@progress/kendo-angular-dialog";
+import { GridModule } from "@progress/kendo-angular-grid";
+
 
 @Component({
   selector: 'app-add-remarks',
   standalone: true,
-  imports: [DialogModule,NgIf,InputsModule],
+  imports: [DialogModule,NgIf,InputsModule,ButtonsModule,GridModule],
   templateUrl: './add-remarks.component.html',
   styleUrl: './add-remarks.component.scss'
 })
 export class AddRemarksComponent  {
-    
- 
-  // public formGroup: FormGroup= this.fb.group({
-  //   age:  0,
-  //   name: 'Rama',
-  // });;
-  // public ngOnInit(): void {
-    // this.formGroup = this.fb.group({
-    //   age: 0,
-    //   name: 'Rama',
-    // });
-  //}
+  
+  public dialogThemeColor: DialogThemeColor = "primary";
+
+  public gridData: Product[] = [
+    {
+      Comment: "Test Comment",
+    }
+  ];
   public opened = false;
 
   public close(): void {
@@ -38,4 +38,7 @@ export class AddRemarksComponent  {
   public open(): void {
     this.opened = true;
   }
+}
+export class Product {
+ public Comment: string = "Test Comment";
 }
