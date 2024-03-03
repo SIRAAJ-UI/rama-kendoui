60c1c6b81bc35523f70730e37bc3f4df9bb5ae90
Sirajudeen <ksiraj2002@gmail.com>
2024-03-03 20:00:36 +0530

    updated version

--- src/app/default/details-page/add-remarks/add-remarks.component.ts
+++ src/app/default/details-page/add-remarks/add-remarks.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit  } from '@angular/core';
+import { Component,OnInit, ViewChild  } from '@angular/core';
 import { DialogService } from '@progress/kendo-angular-dialog';
 import { DialogModule } from '@progress/kendo-angular-dialog';
 import { NgIf } from '@angular/common';
@@ -11,26 +11,51 @@ import { InputsModule } from '@progress/kendo-angular-inputs';
 import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { DialogThemeColor } from "@progress/kendo-angular-dialog";
 import { GridModule } from "@progress/kendo-angular-grid";
+import { LabelModule } from "@progress/kendo-angular-label";
 
 
 @Component({
   selector: 'app-add-remarks',
   standalone: true,
-  imports: [DialogModule,NgIf,InputsModule,ButtonsModule,GridModule],
+  imports: [DialogModule,NgIf,InputsModule,ButtonsModule, LabelModule, GridModule],
   templateUrl: './add-remarks.component.html',
   styleUrl: './add-remarks.component.scss'
 })
 export class AddRemarksComponent  {
   
+  @ViewChild("commentsArea") commentsArea: any;
   public dialogThemeColor: DialogThemeColor = "primary";
-
-  public gridData: Product[] = [
+  public gridData: any[] = [
     {
-      Comment: "Test Comment",
-    }
+      ProductID: 1,
+      ProductName: "Chai",
+      UnitPrice: 18,
+      Category: {
+        CategoryID: 1,
+        CategoryName: "Beverages",
+      },
+    },
+    {
+      ProductID: 2,
+      ProductName: "Chang",
+      UnitPrice: 19,
+      Category: {
+        CategoryID: 1,
+        CategoryName: "Beverages",
+      },
+    },
+    {
+      ProductID: 3,
+      ProductName: "Aniseed Syrup",
+      UnitPrice: 10,
+      Category: {
+        CategoryID: 2,
+        CategoryName: "Condiments",
+      },
+    },
   ];
-  public opened = false;
 
+  public opened = false;
   public close(): void {
     this.opened = false;
   }
@@ -38,7 +63,8 @@ export class AddRemarksComponent  {
   public open(): void {
     this.opened = true;
   }
-}
-export class Product {
- public Comment: string = "Test Comment";
+
+  // ngOnInit() {
+  //   this.commentsArea.nativeElement.trim()
+  // }
 }
