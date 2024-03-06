3332e29dd7fdcda668ea300ef0f0d2329d65597a
Yekula <ramadevi.yekula@acgov.org>
2024-03-05 17:13:45 -0800

    changes pushed today

--- src/app/default/details-page/add-remarks/add-remarks.component.ts
+++ src/app/default/details-page/add-remarks/add-remarks.component.ts
@@ -12,6 +12,7 @@ import { ButtonsModule } from '@progress/kendo-angular-buttons';
 import { DialogThemeColor } from "@progress/kendo-angular-dialog";
 import { GridModule } from "@progress/kendo-angular-grid";
 import { LabelModule } from "@progress/kendo-angular-label";
+import { GridSize } from "@progress/kendo-angular-grid";
 
 
 @Component({
@@ -22,13 +23,14 @@ import { LabelModule } from "@progress/kendo-angular-label";
   styleUrl: './add-remarks.component.scss'
 })
 export class AddRemarksComponent  {
-  
+  public smallSize: GridSize = "small";
+
   @ViewChild("commentsArea") commentsArea: any;
   public dialogThemeColor: DialogThemeColor = "primary";
   public gridData: any[] = [
     {
       commentsID: 1,
-      comments: "Chai"
+      comments: "The Kendo UI for Angular Grid is one of the most powerful data grid components available for Angular developers. Built from the ground up for Angular and with focus on performance, the Angular Data Grid contains must-have features, including"
     },
     {
       commentsID: 2,
@@ -37,6 +39,13 @@ export class AddRemarksComponent  {
     {
       commentsID: 3,
       comments: "Aniseed Syrup"
+    }, {
+      commentsID: 2,
+      comments: "Chang"
+    },
+    {
+      commentsID: 3,
+      comments: "The Kendo UI for Angular Grid is one of the most powerful data grid components available for Angular developers. Built from the ground up for Angular and with focus on performance, the Angular Data Grid contains must-have features, including"
     },
   ];
 
